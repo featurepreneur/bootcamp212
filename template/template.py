291a760daee2f41ebf6846d87a5595b827c25b7b
Raja CSP Raman <raja.csp@gmail.com>
2021-03-13 11:37:01 -0500

    Updates for 2021-03-13

---
+++ template/template.py
@@ -0,0 +1,20 @@
+
+'''
+Created on 
+
+Course work: 
+    Template
+@author: raja
+
+Source:
+    
+'''
+
+# Import necessary modules
+
+def startpy():
+    print('Hey there')
+
+
+if __name__ == '__main__':
+    startpy()
